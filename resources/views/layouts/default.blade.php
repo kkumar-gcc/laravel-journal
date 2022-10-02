50c6e87c2637b2b9b798f016ae432fc0ede05887
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2022-10-02 12:14:31 +0800

    Put milkdown script directly inside layouts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -40,9 +40,8 @@
 
     @livewireScripts
     @stack('scripts')
-    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
-        data-turbolinks-eval="false" data-turbo-eval="false"></script>
-
+        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
+        <x-scripts.milkdown />
 </body>
 
 </html>
